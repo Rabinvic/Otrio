7072f5b0694e9613bc99d1f1847bad91279c6209
Victor Rabinovich <vityarabinovich@gmail.com>
2022-11-16 23:57:42 -0500

    Continuing GUI layout
    Added a title screen, a player selection screen, and disclaimer page.
    The title screen has a start game button, hint, button, disclaimer
    button, image, and authorship label. Player select gives all possible
    number of players.

--- src/gui/OtrioGUI.java
+++ src/gui/OtrioGUI.java
@@ -1,10 +1,17 @@
 package gui;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import model.Observer;
@@ -13,16 +20,24 @@ import model.OtrioModel;
 public class OtrioGUI extends Application implements Observer<OtrioModel>{
 
     private OtrioModel model;
+    private Scene titleScene;
+    private Scene disclaimerScene;
+    private Scene playerSelectionScene;
+    private Scene board;
+    private Stage stage;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        BorderPane bp = new BorderPane();
-        GridPane gp = makGridPane();
-
-        bp.setCenter(gp);
+        this.stage=primaryStage;
+        makeTitleScene();
+        makeBoardScene();
+        makePlayerSelectionScene();
         
-        primaryStage.setScene(new Scene(bp));
-        primaryStage.show();
+        
+        this.stage.setScene(titleScene);
+        this.stage.setTitle("Otrio");
+        this.stage.setResizable(false);
+        this.stage.show();
     }
 
     @Override
@@ -31,11 +46,81 @@ public class OtrioGUI extends Application implements Observer<OtrioModel>{
         model.addObserver(this);
     }
 
+    private void makeBoardScene(){
+        BorderPane bp = new BorderPane();
+        GridPane gp = makGridPane();
+        bp.setCenter(gp);
+        gp.setAlignment(Pos.CENTER);
+        board=new Scene(bp);
+    
+    }
+
+    private void makePlayerSelectionScene(){
+        FlowPane playerSelect=makePlayerSelectFP();
+        playerSelectionScene=new Scene(playerSelect);
+        playerSelect.setAlignment(Pos.CENTER);
+    }
+
+    private void makeTitleScene() throws FileNotFoundException{
+        BorderPane bp= new BorderPane();
+        FlowPane fp=new FlowPane();
+        Label authLabel=new Label("Application by Victor Rabinovich");
+
+        Image image=new Image(new FileInputStream("src/images/otrio.jpeg"));
+        ImageView imageView=new ImageView(image);
+        imageView.setFitWidth(500);
+        imageView.setFitHeight(300);
+        imageView.setPreserveRatio(true);
+
+        Button newGameButton=new Button("New Game");
+        newGameButton.setOnAction(event -> stage.setScene(playerSelectionScene));
+        Button rulesButton=new Button("Rules");
+        Button disclaimerButton=new Button("Disclaimer");
+        disclaimerButton.setOnAction(event -> {makeDisclaimerScene(); stage.setScene(disclaimerScene);});
+        fp.getChildren().addAll(newGameButton,rulesButton,disclaimerButton);
+        bp.setBottom(fp);
+        fp.setAlignment(Pos.BOTTOM_CENTER);
+
+        bp.setCenter(imageView);
+        bp.setTop(authLabel);
+        //imageView.setAlignment(Pos.CENTER);
+
+        titleScene=new Scene(bp);
+    }
+
+    private void makeDisclaimerScene(){
+        BorderPane bp = new BorderPane();
+        Label disclaimer=new Label("Otrio is a Trademark of Spin Master Ltd.\nThis is just made for fun and as aprreciation for the wonderful game they've created");
+        disclaimer.setStyle("-fx-font-size: 24px;-fx-content-display: center;");
+        bp.setCenter(disclaimer);
+
+        Button backButton=new Button("Main Menu");
+        backButton.setOnAction(event -> stage.setScene(titleScene));
+        backButton.setStyle("-fx-font-size: 20px;");
+        bp.setTop(backButton);
+        backButton.setAlignment(Pos.TOP_LEFT);
+        disclaimerScene=new Scene(bp);
+    }
+
+
+    private FlowPane makePlayerSelectFP(){
+        FlowPane fp=new FlowPane();
+        for(int i=2;i<=4;i++){
+            Button b = new Button("Players: "+i);
+            int players=i;
+            b.setOnAction(event -> {model.setPlayers(players); stage.setScene(board);});
+            b.setPrefSize(100, 100);
+            fp.getChildren().add(b);
+        }
+        return fp;
+    }
     private GridPane makGridPane(){
         GridPane gp= new GridPane();
         for(int i=0;i<3;i++){
             for(int j=0;j<3;j++){
                 Button b=new Button();
+                b.setPrefSize(100, 100);
+                b.setStyle("-fx-bacground-color: white;");
                 gp.add(b, j, i);
             }
         }
--- src/model/OtrioModel.java
+++ src/model/OtrioModel.java
@@ -4,14 +4,22 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class OtrioModel {
-    private List<Observer<OtrioModel>> observers = new LinkedList();
+    private List<Observer<OtrioModel>> observers;
     private int players;
 
 
     static int[] player1Pieces, player2Pieces, player3Pieces, player4Pieces;
 
+    public OtrioModel(){
+        observers = new LinkedList<Observer<OtrioModel>>();
+    }
+
     public void addObserver(Observer<OtrioModel> observer){
         observers.add(observer);
+    }
+
+    public void setPlayers(int numPlayers){
+        this.players=numPlayers;
     }
 
     
